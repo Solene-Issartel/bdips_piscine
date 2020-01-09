f81df69cc0f9494cc6e3255f90e1f9b3fc0454c9
Axel Duval <axel.duval74@gmail.com>
2020-01-09 16:13:15 +0100

    Modification design session et quiz

--- resources/views/questionnaire/result_quiz.blade.php
+++ resources/views/questionnaire/result_quiz.blade.php
@@ -5,16 +5,14 @@ Result
 @endsection
 
 @section('content')
-    <!-- Creation du container-->
-    <div class="container-fluid">
-      <!-- Titre-->
-      <div class="row welcome text-center padding">
-        <div class="col-12">
-          <h1 class="display-4">Result</h1>
-          <hr class="padding">
-        </div>
-      </div>
-      <h1>{{$score_final}}</h1>
-      <h1>/990</h1>
-      </div>
+<!-- Titre -->
+<div class="container-fluid text-center pad">
+  <h1 class="display-4">Result</h1>
+  <hr style="border-top: 2px solid #b4b4b4; width: 25%; margin-top: .9rem; margin-bottom: 1rem;">
+</div>
+
+<!-- Creation du container-->
+<h1>{{$score_final}}</h1>
+<h1>/990</h1>
+
 @endsection

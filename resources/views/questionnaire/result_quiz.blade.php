fe4cc46f7b77086fe5efdb29401d97f5e48f8599
Axel Duval <axel.duval74@gmail.com>
2020-01-11 18:13:45 +0100

    Modification page resultat

--- resources/views/questionnaire/result_quiz.blade.php
+++ resources/views/questionnaire/result_quiz.blade.php
@@ -5,18 +5,16 @@ Result
 @endsection
 
 @section('content')
-<!-- Titre -->
-<div class="container-fluid text-center pad">
-  <h1 class="display-4">Your score</h1>
-  <hr style="border-top: 2px solid #b4b4b4; width: 21%; margin-top: .9rem; margin-bottom: 1rem;">
+<!-- Congrats -->
+<div class="container-fluid">
+  <img src="../public/img/congrats.png" class="img-fluid">
 </div>
-
 <!-- Creation du container pour le score-->
 <div class="container-fluid">
-  <div class="row text-center">
-    <div class="col-sm-5"></div>
-    <div class="col-sm-2" style="margin-top:60px;">
-      <h1 style="font-size:80px;">{{$score_final}}</h1>
+  <div class="row">
+    <div class="col-sm-4"></div>
+    <div class="col-sm-4 text-center">
+      <h1 style="font-size:200px;color:#E3F1FC;">{{$score_final}}/990</h1>
     </div>
   </div>
 </div>

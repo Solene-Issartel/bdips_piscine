b4c7022f80c32bf4bc0480da04324d18afa69d5d
Axel Duval <axel.duval74@gmail.com>
2020-01-12 12:51:27 +0100

    modif design

--- resources/views/check.blade.php
+++ resources/views/check.blade.php
@@ -1,22 +1,27 @@
-@extends('layouts.app')
+@extends('layouts.layauth')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Please check your email to validate your account !
-                </div>
+<div class="container-fluid row pad">
+    <!-- Colone 1 -->
+    <div class="col-sm-6">
+        <div class="container-fluid">
+            <div class="panel-body text-center" style="vertical-align: middle;">
+                <!-- Titre -->
+                <h1 class="display-4" style="margin-top:22%;">Waiting for you</h1>
+                </br>
+                <h5>- Please check your mailbox -</h5>
+                @if (session('status'))
+                    <div class="alert alert-success text-center" style="width:70%;height:auto;margin-left:15%;margin-top:30px;">
+                        {{ session('status') }}
+                    </div>
+                @endif
             </div>
+        </div>
+    </div>
+    <!-- Colone 2 (image) -->
+    <div class="col-sm-6">
+        <div class="container-fluid">
+            <img src="../public/img/signIn.png" class="img-fluid">
         </div>
     </div>
 </div>

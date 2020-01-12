aaaa318dd2be290320c427dc308fb2c0e3cdbffc
Axel Duval <axel.duval74@gmail.com>
2020-01-12 12:27:37 +0100

    modif design lost password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layauth')
 
 @section('content')
-<div class="container-fluid text-center">
+<div class="container-fluid text-center" style="margin-top:15%;">
     <div class="row">
         <div class="col-sm-6">
             <div class="container-fluid">
@@ -10,37 +10,29 @@
         </div>
         <div class="col-sm-6">
             <!-- Titre -->
-            <div class="container-fluid text-center pad">
+            <div class="container-fluid text-center" style="margin-top:30px;">
                 <h1 class="display-4">Reset password</h1>
                 <hr style="border-top: 2px solid #b4b4b4; width: 50%; margin-top: .9rem; margin-bottom: 1rem;">
+                <div class="row text-center" style="width:70%;height:auto;margin-left:15%;">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                </div>
 
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
-
-                <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
+                <form class="form-horizontal" style="margin-top:50px;width:50%;height:auto;margin-left:25%;" method="POST" action="{{ route('password.email') }}">
                         {{ csrf_field() }}
 
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-
-                        <div class="col-md-6">
-                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Enter your account e-mail" required>
-
-                            @if ($errors->has('email'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                            @endif
-                        </div>
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">                        
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Enter your account e-mail" required>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif                       
                     </div>
-
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            <button type="submit" class="btn btn-primary">Send</button>
-                        </div>
-                    </div>
+                    <button type="submit" class="btn btn-primary">Send</button>                    
                 </form>
 
             </div>

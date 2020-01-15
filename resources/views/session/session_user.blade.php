737ac61214ec3aa622046d2c9cef717309979752
Solene34 <44373961+Solene34@users.noreply.github.com>
2020-01-15 19:18:07 +0100

    fix sessions list

--- resources/views/session/session_user.blade.php
+++ resources/views/session/session_user.blade.php
@@ -20,7 +20,7 @@ Session User
         <form class="form-horizontal" method="POST" action="{{ url('waiting_session')}}">
                         {{ csrf_field() }}
           
-            @if (isset($sujets))
+            @if (count($sujets)==0)
               <div class="form-group">
                 <select class="form-control" id="form-session" name="id_session">
                   @foreach($sujets as $sujet)
